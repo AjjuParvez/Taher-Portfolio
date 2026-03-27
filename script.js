e2a085de7eab74ddc32a385625a7e775a5453312
AjjuParvez <ajjuparvez456@gmail.com>
2026-03-27 15:39:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 document.querySelectorAll("a[href^='#']").forEach(link => {
     link.addEventListener("click", function(e) {
         e.preventDefault();
-
         const target = document.querySelector(this.getAttribute("href"));
         if (target) {
             target.scrollIntoView({ behavior: "smooth" });
