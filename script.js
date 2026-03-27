8c0a72a36f7b20286203f1d39de53ba40091750d
AjjuParvez <ajjuparvez456@gmail.com>
2026-03-27 15:27:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Smooth scrolling
-document.querySelectorAll("a[href^='#']").forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
+document.querySelectorAll("a[href^='#']").forEach(link => {
+    link.addEventListener("click", function(e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute("href"))
-        .scrollIntoView({ behavior: "smooth" });
+
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            target.scrollIntoView({ behavior: "smooth" });
+        }
     });
 });
